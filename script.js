b9f8dd283803745f9a71bbe72cb914a93840c0d7
lorele123456 <evlorele1@gmail.com>
2026-09-18 13:24:15 -0500

    ACTU

--- script.js
+++ script.js
@@ -368,7 +368,7 @@ async function cargarBanners() {
     }
 }
 function enviarWhatsApp() {
-    let msg = `*PEDIDO PIETRA & CO.*\n\n`;
+    let msg = `*PEDIDO NARIUSHKHA SHOP*\n\n`;
     carrito.forEach(p => msg += `• ${p.nombre} (S/ ${p.precio.toFixed(2)})\n`);
     msg += `\n*TOTAL CON ENVÍO:* S/ ${document.getElementById('total-final').innerText}`;
     window.open(`https://wa.me/${NUMERO_WA}?text=${encodeURIComponent(msg)}`);
